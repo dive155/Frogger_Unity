aeb390c4438fedf12425d695b4051212359daad7
PETERSBURG\Dmitrii_Valiaev1 <Dmitrii_Valiaev1@epam.com>
2018-01-11 13:03:56 +0300

    Android support added

--- Assets/Scripts/FrogControl.cs
+++ Assets/Scripts/FrogControl.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class FrogControl : MonoBehaviour {
 	private float speed;
 	private Vector3 rotDest;
 
 	private bool alive;
+
+    [SerializeField] private Transform body;
 
 	void Start () {
 		speed = 7;
@@ -16,7 +19,7 @@ public class FrogControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (alive) {
-			if (Input.GetKey ("d")) {
+			/*if (Input.GetKey ("d")) {
 				transform.Translate (-speed * Time.deltaTime, 0, 0);
 			} else if (Input.GetKey ("a")) {
 				transform.Translate (speed * Time.deltaTime, 0, 0);
@@ -25,6 +28,12 @@ public class FrogControl : MonoBehaviour {
 			} else if (Input.GetKey ("w")) {
 				transform.Translate (0, 0, -speed * Time.deltaTime);
 			}
+            */
+            transform.Translate (-speed * Time.deltaTime * CrossPlatformInputManager.GetAxis("Horizontal"), 0, 0);
+            transform.Translate (0, 0, -speed * Time.deltaTime * CrossPlatformInputManager.GetAxis("Vertical"));
+
+            rotDest = new Vector3 (CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical"));
+            body.transform.rotation = Quaternion.Lerp(body.transform.rotation, Quaternion.LookRotation(rotDest), 0.3f);
 		}
 	}
 
